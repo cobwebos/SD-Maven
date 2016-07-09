chrisdutz/maven

[MNG-6056] Implement Feature Toggle Module to handle Feature Toggles  o Implemented feature toggle module plus command line options    etc. to activate feature and list available feature toggles.  o Added example feature locations which should be removed before    really using it.

maven-core/src/main/java/org/apache/maven/graph/DefaultGraphBuilder.java
@@ -36,6 +36,8 @@ import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenExecutionResult;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.ProjectDependencyGraph;
+import org.apache.maven.feature.AvailableFeatures;
+import org.apache.maven.feature.SelectedFeatures;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.building.DefaultModelProblem;
 import org.apache.maven.model.building.ModelProblem;
@@ -65,6 +67,10 @@ public class DefaultGraphBuilder
 
     @Requirement
     protected ProjectBuilder projectBuilder;
+    
+    @Requirement
+    private SelectedFeatures selectedFeatures;
+    
 
     @Override
     public Result<ProjectDependencyGraph> build( MavenSession session )
@@ -74,6 +80,10 @@ public class DefaultGraphBuilder
             return dependencyGraph( session, session.getProjects(), false );
         }
         
+        if (selectedFeatures.isFeatureActive( AvailableFeatures.MNG10000 )) {
+            logger.info( " -> Features MNG 10000 is activated." );
+        }
+
         List<MavenProject> projects = session.getProjects();
 
         if ( projects == null )

maven-embedder/src/test/java/org/apache/maven/cli/MavenCliTest.java
@@ -24,6 +24,7 @@ import java.io.File;
 import junit.framework.TestCase;
 
 import org.apache.commons.cli.ParseException;
+import org.apache.maven.feature.AvailableFeatures;
 
 public class MavenCliTest
     extends TestCase
@@ -107,4 +108,5 @@ public class MavenCliTest
 
         }
     }
+    
 }

cobwebos/SD-Maven

maven-core/src/main/java/org/apache/maven/graph/DefaultGraphBuilder.java
@@ -36,6 +36,8 @@ import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.execution.MavenExecutionResult;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.ProjectDependencyGraph;
+import org.apache.maven.feature.AvailableFeatureToggles;
+import org.apache.maven.feature.FeatureToggles;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.building.DefaultModelProblem;
 import org.apache.maven.model.building.ModelProblem;
@@ -65,6 +67,10 @@ public class DefaultGraphBuilder
 
     @Requirement
     protected ProjectBuilder projectBuilder;
+    
+    @Requirement
+    private FeatureToggles selectedFeatures;
+    
 
     @Override
     public Result<ProjectDependencyGraph> build( MavenSession session )
@@ -74,6 +80,10 @@ public class DefaultGraphBuilder
             return dependencyGraph( session, session.getProjects(), false );
         }
         
+        if (selectedFeatures.isToggleActive( AvailableFeatureToggles.MNG10000 )) {
+            logger.info( " -> Features MNG 10000 is activated." );
+        }
+
         List<MavenProject> projects = session.getProjects();
 
         if ( projects == null )

maven-embedder/src/test/java/org/apache/maven/cli/MavenCliTest.java
@@ -24,6 +24,7 @@ import java.io.File;
 import junit.framework.TestCase;
 
 import org.apache.commons.cli.ParseException;
+import org.apache.maven.feature.AvailableFeatureToggles;
 
 public class MavenCliTest
     extends TestCase
@@ -107,4 +108,5 @@ public class MavenCliTest
 
         }
     }
+    
 }