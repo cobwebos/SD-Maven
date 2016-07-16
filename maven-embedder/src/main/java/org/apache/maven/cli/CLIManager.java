3912ecef3ebc7b9ad055f4aa04b67ee654b1366b
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-07-16 13:13:54 +0200

    [MNG-6012] Missing profile is only notified at the end of a run  o Introduced new command line option --fail-on-missing-profiles    which will fail the build immediately.  o Print WARNING at the beginning and at the end of the output.

--- maven-embedder/src/main/java/org/apache/maven/cli/CLIManager.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/CLIManager.java
@@ -80,6 +80,8 @@ public class CLIManager
     public static final String FAIL_AT_END = "fae";
 
     public static final String FAIL_NEVER = "fn";
+    
+    public static final String FAIL_ON_MISSING_PROFILES = "fomp";
 
     public static final String RESUME_FROM = "rf";
 
@@ -140,6 +142,7 @@ public class CLIManager
         options.addOption( OptionBuilder.withLongOpt( "threads" ).hasArg().withDescription( "Thread count, for instance 2.0C where C is core multiplied" ).create( THREADS ) );
         options.addOption( OptionBuilder.withLongOpt( "legacy-local-repository" ).withDescription( "Use Maven 2 Legacy Local Repository behaviour, ie no use of _remote.repositories. Can also be activated by using -Dmaven.legacyLocalRepo=true" ).create( LEGACY_LOCAL_REPOSITORY ) );
         options.addOption( OptionBuilder.withLongOpt( "builder" ).hasArg().withDescription( "The id of the build strategy to use" ).create( BUILDER ) );
+        options.addOption( OptionBuilder.withLongOpt( "fail-on-missing-profiles" ).withDescription( "The build will fail if you are trying to use one or more profiles which do not exist." ).create( FAIL_ON_MISSING_PROFILES ) );
 
         // Adding this back in for compatibility with the verifier that hard codes this option.
         options.addOption( OptionBuilder.withLongOpt( "no-plugin-registry" ).withDescription( "Ineffective, only kept for backward compatibility" ).create( "npr" ) );
