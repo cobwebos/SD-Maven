3b6daf278d655e2e077e5c76720f144b6b8178af
Christian Schulte <schulte@apache.org>
2016-07-30 20:21:09 +0200

    [MNG-6073] Addition of a core extension point to the model builder supporting model finalization. [MNG-3825] Dependencies with classifier should not always require a version.
    o Updated to add a missing '@since' tag.

--- maven-model-builder/src/main/java/org/apache/maven/model/building/DefaultModelBuilder.java
+++ maven-model-builder/src/main/java/org/apache/maven/model/building/DefaultModelBuilder.java
@@ -233,6 +233,9 @@ public class DefaultModelBuilder
         return this;
     }
 
+    /**
+     * @since 3.4
+     */
     public DefaultModelBuilder setReportingConverter( ReportingConverter reportingConverter )
     {
         this.reportingConverter = reportingConverter;
