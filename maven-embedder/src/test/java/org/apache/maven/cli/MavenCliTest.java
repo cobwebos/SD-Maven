chrisdutz/maven

[MNG-6056] Implement Feature Toggle Module to handle Feature Toggles  o Implemented feature toggle module plus command line options    etc. to activate feature and list available feature toggles.  o Added example feature locations which should be removed before    really using it.

maven-embedder/src/test/java/org/apache/maven/cli/MavenCliTest.java
@@ -24,6 +24,7 @@ import java.io.File;
 import junit.framework.TestCase;
 
 import org.apache.commons.cli.ParseException;
+import org.apache.maven.feature.AvailableFeatures;
 
 public class MavenCliTest
     extends TestCase
@@ -107,4 +108,5 @@ public class MavenCliTest
 
         }
     }
+    
 }

cobwebos/SD-Maven

maven-embedder/src/test/java/org/apache/maven/cli/MavenCliTest.java
@@ -22,6 +22,7 @@ package org.apache.maven.cli;
 import java.io.File;
 
 import org.apache.commons.cli.ParseException;
+import org.apache.maven.feature.AvailableFeatureToggles;
 
 import junit.framework.TestCase;
 